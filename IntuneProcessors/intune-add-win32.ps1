0c362d2c5c5e44fe83cfc55df4211b99039e463a
kahosks <ka.ho@hotmail.com>
2024-08-12 11:33:35 -0600

    Updated IntuneProcessors and powershell scripts

---
+++ IntuneProcessors/intune-add-win32.ps1
@@ -0,0 +1,30 @@
+Set-PSRepository PSGallery -InstallationPolicy Trusted
+Install-Module -Name "IntuneWin32App" 
+
+$ClientID= $args[0]
+$ClientSecret= $args[1]
+$TenantID= $args[2]
+$AppName= $args[3]
+$IntuneWinFile= $args[4]
+$CheckRoot= $args[5]
+$CheckTarget= $args[6]
+$InstallCommand= $args[7]
+$UninstallCommand= $args[8]
+$Description= $args[9]
+$Publisher= $args[10]
+$AppVersion= $args[11]
+
+Write-Output "AppName: $AppName
+IntuneWinFile: $IntuneWinFile
+CheckRoot: $CheckRoot
+CheckTarget: $CheckTarget
+InstallCommand: $InstallCommand
+UninstallCommand: $UninstallCommand
+Description: $Description
+Publisher: $Publisher
+AppVersion: $AppVersion"
+
+Connect-MSIntuneGraph -TenantID $TenantID -ClientID $ClientID -ClientSecret $ClientSecret
+$RequirementRule = New-IntuneWin32AppRequirementRule -Architecture "x64" -MinimumSupportedWindowsRelease "W10_1607"
+$DetectionRule = New-IntuneWin32AppDetectionRuleFile -Existence -Path $CheckRoot -FileOrFolder $CheckTarget -DetectionType "exists"
+Add-IntuneWin32App -FilePath $IntuneWinFile -DisplayName $AppName -Description $Description -Publisher $Publisher -AppVersion $AppVersion -InstallExperience "system" -RestartBehavior "suppress" -DetectionRule $DetectionRule -RequirementRule $RequirementRule -InstallCommandLine $InstallCommand -UninstallCommandLine $UninstallCommand
--- IntuneProcessors/intune-package-win32.ps1
+++ IntuneProcessors/intune-package-win32.ps1
@@ -1,6 +1,10 @@
+Set-PSRepository PSGallery -InstallationPolicy Trusted
+Install-Module -Name SvRooij.ContentPrep.Cmdlet
+
 $Source= $args[0]
 $Destination = $args[1]
 $FileName = $args[2]
+
 
 Write-Output "Begin Packaging..."
 New-IntuneWinPackage -SourcePath $Source -DestinationPath $Destination -SetupFile $FileName
