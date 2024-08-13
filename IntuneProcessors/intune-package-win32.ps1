1db8a8681304325f71fb2fde2b5dcdb6b88fc9fb
Steven Sieu <ka.ho@hotmail.com>
2024-08-13 16:50:23 -0600

    Swap Packaging to use IntuneWin32App

--- IntuneProcessors/intune-package-win32.ps1
+++ IntuneProcessors/intune-package-win32.ps1
@@ -1,11 +1,10 @@
 Set-PSRepository PSGallery -InstallationPolicy Trusted
-Install-Module -Name SvRooij.ContentPrep.Cmdlet
+Install-Module -Name "IntuneWin32App" 
 
 $Source= $args[0]
 $Destination = $args[1]
 $FileName = $args[2]
 
-
 Write-Output "Begin Packaging..."
-New-IntuneWinPackage -SourcePath $Source -DestinationPath $Destination -SetupFile $FileName
+New-IntuneWin32AppPackage -SourcePath $Source -DestinationPath $Destination -SetupFile $FileName
 Write-Output "Packaging Complete"
