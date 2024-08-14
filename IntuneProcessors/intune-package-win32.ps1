08ec367766f63bddd167d4f07ae677ae179682f6
Steven Sieu <ka.ho@hotmail.com>
2024-08-14 09:31:54 -0600

    Use Invoke-WebRequest

--- IntuneProcessors/intune-package-win32.ps1
+++ IntuneProcessors/intune-package-win32.ps1
@@ -2,8 +2,9 @@ $Source= $args[0]
 $Destination = $args[1]
 $FileName = $args[2]
 
+Write-Output "Acquiring Win32 Content Prep Tool"
+Invoke-WebRequest -Uri "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/raw/master/IntuneWinAppUtil.exe" -Outfile "IntuneWinAppUtil.exe"
+
 Write-Output "Begin Packaging..."
-Start-DownloadFile -URL "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/raw/master/IntuneWinAppUtil.exe" -Name "IntuneWinAppUtil.exe"
-ls
 & ".\IntuneWinAppUtil.exe" -c $Source -s $FileName -o $Destination -q
 Write-Output "Packaging Complete"
