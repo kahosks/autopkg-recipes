d6664ac4d4d71db5187ddc038423a1230eafd236
Steven Sieu <ka.ho@hotmail.com>
2024-08-14 09:22:42 -0600

    Run IntuneWinAppUtil.exe directly with &

--- IntuneProcessors/intune-package-win32.ps1
+++ IntuneProcessors/intune-package-win32.ps1
@@ -3,8 +3,7 @@ $Destination = $args[1]
 $FileName = $args[2]
 
 Write-Output "Begin Packaging..."
-Start-DownloadFile -URL "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/raw/master/IntuneWinAppUtil.exe" -Path $env:TEMP -Name "IntuneWinAppUtil.exe"
-$IntuneWinAppUtilPath = Join-Path -Path $env:TEMP -ChildPath "IntuneWinAppUtil.exe"
-$PackageInvocation = Invoke-Executable -FilePath $IntuneWinAppUtilPath -Arguments "-c ""$($Source)"" -s ""$($FileName)"" -o ""$($Destination)"" -q" -CreateNoWindow $false -UseShellExecute $true
+Start-DownloadFile -URL "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/raw/master/IntuneWinAppUtil.exe" -Name "IntuneWinAppUtil.exe"
+& ".\IntuneWinAppUtil.exe" -c $Source -s $FileName -o $Destination -q
 
 Write-Output "Packaging Complete"
