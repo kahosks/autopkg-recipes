5565471fc00c655c318bc45e0b07b80b9adb3456
Steven Sieu <ka.ho@hotmail.com>
2024-08-14 08:41:40 -0600

    Fixed arguements

--- IntuneProcessors/intune-package-win32.ps1
+++ IntuneProcessors/intune-package-win32.ps1
@@ -5,6 +5,6 @@ $FileName = $args[2]
 Write-Output "Begin Packaging..."
 Start-DownloadFile -URL "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/raw/master/IntuneWinAppUtil.exe" -Path $env:TEMP -Name "IntuneWinAppUtil.exe"
 $IntuneWinAppUtilPath = Join-Path -Path $env:TEMP -ChildPath "IntuneWinAppUtil.exe"
-$PackageInvocation = Invoke-Executable -FilePath $IntuneWinAppUtilPath -Arguments "-c ""$($SourceFolder)"" -s ""$($SetupFile)"" -o ""$($OutPutFolder)"" -q" -RedirectStandardOutput $false -RedirectStandardError $false -CreateNoWindow $false -UseShellExecute $true
+$PackageInvocation = Invoke-Executable -FilePath $IntuneWinAppUtilPath -Arguments "-c ""$($Source)"" -s ""$($FileName)"" -o ""$($Destination)"" -q" -CreateNoWindow $false -UseShellExecute $true
 
 Write-Output "Packaging Complete"
