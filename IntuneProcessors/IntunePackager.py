d6549a9874dad8f210a16318b489829acae8f986
Steven Sieu <ka.ho@hotmail.com>
2024-08-14 12:38:42 -0600

    create command

--- IntuneProcessors/IntunePackager.py
+++ IntuneProcessors/IntunePackager.py
@@ -35,8 +35,15 @@ class IntunePackager(Processor):
         try:
             urlretrieve("https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/raw/master/IntuneWinAppUtil.exe","IntuneWinAppUtil.exe")
             os.makedirs(self.env.get("destination_path"), exist_ok=True)
+            command = [
+                "IntuneWinAppUtil.exe",
+                '-c', self.env.get("source_path"),
+                '-s', self.env.get("setup_file"),
+                '-o', self.env.get("destination_path"),
+                '-q'
+            ]
             #r = subprocess.Popen([self.env.get("powershell_path"), "IntuneProcessors/intune-package-win32.ps1", self.env.get("source_path"), self.env.get("destination_path"), self.env.get("setup_file")], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            r = subprocess.run(["IntuneWinAppUtil.exe","-c \"{}\"".format(self.env.get("source_path")),"-s \"{}\"".format(self.env.get("setup_file")),"-o \"{}\"".format(self.env.get("destination_path")), "-q"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            r = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             print(r.stdout.read().decode().strip())
             file_name = self.env.get("destination_path") + "\\" + self.env.get("setup_file").replace(".exe", ".intunewin")
             print("Confirming " + file_name + " exists...")
