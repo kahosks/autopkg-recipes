90c8dd26863ffcc9c3e77a70ec3c9347ca0135bd
Steven Sieu <ka.ho@hotmail.com>
2024-08-14 09:24:21 -0600

    ls debug

--- IntuneProcessors/intune-package-win32.ps1
+++ IntuneProcessors/intune-package-win32.ps1
@@ -4,6 +4,6 @@ $FileName = $args[2]
 
 Write-Output "Begin Packaging..."
 Start-DownloadFile -URL "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/raw/master/IntuneWinAppUtil.exe" -Name "IntuneWinAppUtil.exe"
+ls
 & ".\IntuneWinAppUtil.exe" -c $Source -s $FileName -o $Destination -q
-
 Write-Output "Packaging Complete"
